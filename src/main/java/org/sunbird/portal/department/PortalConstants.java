c1814156b8e110235b7478b72211a7b540e8bf18
karthik-tarento <karthikeyan.rajendran@tarento.com>
2021-04-30 16:15:27 +0530

    Fix for new Frac roles. Added basic implementation for Cohorts

--- src/main/java/org/sunbird/portal/department/PortalConstants.java
+++ src/main/java/org/sunbird/portal/department/PortalConstants.java
@@ -8,6 +8,7 @@ public class PortalConstants {
 	private PortalConstants() {
 		throw new IllegalStateException("PortalConstants class");
 	}
+
 	public static final String MDO_ROLE_NAME = "MDO ADMIN";
 	public static final String MDO_DEPT_TYPE = "MDO";
 	public static final String SPV_ROLE_NAME = "SPV ADMIN";
@@ -15,5 +16,6 @@ public class PortalConstants {
 	public static final String CBP_DEPT_TYPE = "CBP";
 	public static final String CBP_ROLE_NAME = "CBP ADMIN";
 	public static final List<String> CBP_ROLES = Arrays.asList("EDITOR", "REVIEWER", "PUBLISHER", "CONTENT_CREATOR");
-	public static final List<String> FRAC_ROLES = Arrays.asList("IFUMember", "fracAdmin");
+	public static final List<String> FRAC_ROLES = Arrays.asList("IFUMember", "fracAdmin", "fracReviewerOne",
+			"fracReviewerTwo");
 }
