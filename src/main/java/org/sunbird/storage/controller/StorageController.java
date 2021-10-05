9537727098d9120263941321d47e57bc3672402d
sreeragksgh <sreeragsajeesh@gmail.com>
2021-10-05 10:26:24 +0530

    storage package

--- src/main/java/org/sunbird/storage/controller/StorageController.java
+++ src/main/java/org/sunbird/storage/controller/StorageController.java
@@ -2,6 +2,8 @@ package org.sunbird.storage.controller;
 
 import java.io.File;
 import java.io.FileOutputStream;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.sunbird.storage.service.StorageServiceImpl;
 
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -28,14 +31,29 @@ public class StorageController {
 
     @PostMapping("/upload")
     public ResponseEntity<Map<String, String>> upload(@RequestParam(value = "file", required = true) MultipartFile multipartFile
-                                                   ) throws IOException {
-        String folderName= cbExtServerProperties.getAzureContainerName();
+    ) throws IOException {
+        String folderName = cbExtServerProperties.getAzureContainerName();
         File file = new File(multipartFile.getOriginalFilename());
-            file.createNewFile();
-            FileOutputStream fos = new FileOutputStream(file);
-            fos.write(multipartFile.getBytes());
-            fos.close();
-            Map<String, String> uploadedFile = storageService.uploadFile(folderName, file);
-            file.delete();
+        file.createNewFile();
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(multipartFile.getBytes());
+        fos.close();
+        Map<String, String> uploadedFile = storageService.uploadFile(folderName, file);
+        file.delete();
         return new ResponseEntity<>(uploadedFile, HttpStatus.OK);
-    }}
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Map<String, String>>  deleteCloudFile(@RequestParam(value = "fileName", required = true) String fileName)
+            throws JsonProcessingException {
+            Boolean deleted = storageService.deleteFile(fileName);
+        Map<String, String> deleteFile = new HashMap<>();
+        deleteFile.put("name", fileName);
+            if (deleted) {
+                deleteFile.put("status", "deleted");
+                return new ResponseEntity<>(deleteFile,HttpStatus.OK);
+            }
+        deleteFile.put("status", "not deleted");
+        return new ResponseEntity<>(deleteFile,HttpStatus.FAILED_DEPENDENCY);
+    }
+}
--- src/main/java/org/sunbird/storage/service/StorageServiceImpl.java
+++ src/main/java/org/sunbird/storage/service/StorageServiceImpl.java
@@ -54,4 +54,15 @@ public class StorageServiceImpl {
         }
 
     }
+
+
+    public Boolean deleteFile(String fileName) {
+        try {
+            storageService.deleteObject(cbExtServerProperties.getAzureContainerName(), fileName, Option.apply(Boolean.FALSE));
+            return Boolean.TRUE;
+        } catch (Exception e) {
+            log.error("deleteFile: ", e);
+            return Boolean.FALSE;
+        }
+    }
 }
