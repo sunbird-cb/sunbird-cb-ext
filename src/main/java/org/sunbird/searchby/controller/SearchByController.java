2bf5f9fb08f522b4b2ba29894d65f41af53e0db8
karthik-tarento <karthikeyan.rajendran@tarento.com>
2022-01-27 12:28:25 +0530

    Fixed issue in SearchBy Provider and clean-up based on review comments.

--- src/main/java/org/sunbird/searchby/controller/SearchByController.java
+++ src/main/java/org/sunbird/searchby/controller/SearchByController.java
@@ -1,16 +1,11 @@
 package org.sunbird.searchby.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-import org.sunbird.searchby.model.SearchByFilter;
 import org.sunbird.searchby.service.SearchByService;
 
 @RestController
@@ -19,21 +14,11 @@ public class SearchByController {
 	@Autowired
 	private SearchByService searchByService;
 
+	//v1/readCompetency
 	@GetMapping("/v1/browseByCompetency")
 	public ResponseEntity<?> browseByCompetency(@RequestHeader("x-authenticated-user-token") String authUserToken)
 			throws Exception {
 		return new ResponseEntity<>(searchByService.getCompetencyDetails(authUserToken), HttpStatus.OK);
-	}
-
-	@PostMapping("/v1/browseByCompetency")
-	public ResponseEntity<?> browseByCompetencyByFilter(
-			@RequestHeader("x-authenticated-user-token") String authUserToken,
-			@Valid @RequestBody SearchByFilter filter) throws Exception {
-		if (filter == null) {
-			throw new Exception("Invalid Request");
-		}
-		return new ResponseEntity<>(searchByService.getCompetencyDetailsByFilter(authUserToken, filter),
-				HttpStatus.OK);
 	}
 
 	@GetMapping("/v1/browseByProvider")
