d9a6754b171d27d4855895b256924af782dd4634
Juhi Agarwal <juhiagl8@gmail.com>
2022-05-25 12:38:03 +0530

    changes for the email notification to exclude users

--- src/main/java/org/sunbird/scheduler/util/SchedulerManager.java
+++ src/main/java/org/sunbird/scheduler/util/SchedulerManager.java
@@ -1,55 +1,52 @@
 package org.sunbird.scheduler.util;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.stereotype.Component;
 import org.sunbird.common.util.Constants;
 import org.sunbird.common.util.PropertiesCache;
 import org.sunbird.core.logger.CbExtLogger;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
-import static org.sunbird.common.util.Constants.*;
-
-/** @author Manzarul All the scheduler job will be handle by this class. */
 @Component
-public class SchedulerManager<aDate> {
+public class SchedulerManager {
 
-    private static final int PAGE_DATA_TTL = 4;
     static PropertiesCache p = PropertiesCache.getInstance();
-    private static final Long SCHEDULER_TIME_GAP = Long.valueOf(p.getProperty(Constants.SCHEDULER_TIME_GAP));
     public static ScheduledExecutorService service = ExecutorManager.getExecutorService();
     private static CbExtLogger logger = new CbExtLogger(SchedulerManager.class.getName());
 
-    /** all scheduler job will be configure here. */
+    /** all scheduler job will be configured here. */
     public static void schedule() {
+        logger.info("SchedulerManager:schedule: Started scheduler job for email notifications.");
+        // Today's Date Calculation
+        Calendar todayCal = Calendar.getInstance();
+        todayCal.setTime(new Date());
+        logger.debug("Today's Calendar Date : " + todayCal.getTime().toString());
 
-        logger.info("SchedulerManager:schedule: Started scheduler job for cache refresh.");
-        Map<Integer, Integer> dayToDelay = new HashMap<Integer, Integer>();
-        Calendar with = Calendar.getInstance();
-        Date aDate = new Date();
-        with.setTime(aDate);
-        dayToDelay.put(Calendar.FRIDAY, 6);
-        dayToDelay.put(Calendar.SATURDAY, 5);
-        dayToDelay.put(Calendar.SUNDAY, 4);
-        dayToDelay.put(Calendar.MONDAY, 3);
-        dayToDelay.put(Calendar.TUESDAY, 2);
-        dayToDelay.put(Calendar.WEDNESDAY, 1);
-        dayToDelay.put(Calendar.THURSDAY, 0);
-        int dayOfWeek = with.get(Calendar.DAY_OF_WEEK);
-        int hour = with.get(Calendar.HOUR_OF_DAY);
-        int delayInDays = dayToDelay.get(dayOfWeek);
-        int delayInHours = 0;
-        if(delayInDays == 6 && hour<11){
-            delayInHours = 11 - hour;
-        }else {
-            delayInHours = delayInDays * 24 + ((24 - hour) + 11);
+        // Next Scheduler Run Date & Time Calculation
+        Calendar targetCal = Calendar.getInstance();
+
+        // Setting 6 - 6th Day of the Week : Friday
+        targetCal.set(Calendar.DAY_OF_WEEK, Integer.parseInt(p.getProperty(Constants.SCHEDULER_RUN_DAY)));
+
+        // can be 0-23 - Eg: For Evening 5PM it will be 17
+        targetCal.set(Calendar.HOUR_OF_DAY, Integer.parseInt(p.getProperty(Constants.SCHEDULER_RUN_TIME)));
+
+        // Setting 0 - For the Minute Value of 05:00
+        targetCal.set(Calendar.MINUTE, 0);
+
+        // Setting 0 - For the Second Value of 05:00:00
+        targetCal.set(Calendar.SECOND, 0);
+        logger.debug("Target Calendar Date : " + targetCal.getTime().toString());
+
+        if (todayCal.after(targetCal)) {
+            targetCal.set(Calendar.WEEK_OF_YEAR, targetCal.getWeeksInWeekYear() + 1);
         }
-        service.scheduleWithFixedDelay(new EmailNotificationService(), 0,
-                24, TimeUnit.HOURS);
+
+        service.scheduleWithFixedDelay(new EmailNotificationService(), ((targetCal.getTimeInMillis() - todayCal.getTimeInMillis()) / 1000) / 60,
+                Long.valueOf(p.getProperty(Constants.SCHEDULER_TIME_GAP)), TimeUnit.MINUTES);
         logger.info("SchedulerManager:schedule: Started weekly job for incomplete course email to the users.");
     }
 }
