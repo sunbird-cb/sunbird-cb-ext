f10d9edc66164e89cb737fa90b0e7801fec28bae
Sreerag K S <58926794+sreeragksgh@users.noreply.github.com>
2022-10-12 15:23:50 +0530

    auto enrollment batch API Fixes (#145)
    * auto enrollment batch API Fixes

--- src/main/java/org/sunbird/common/service/ContentServiceImpl.java
+++ src/main/java/org/sunbird/common/service/ContentServiceImpl.java
@@ -1,9 +1,6 @@
 package org.sunbird.common.service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -173,10 +170,25 @@ public class ContentServiceImpl implements ContentService {
 
 		return null;
 	}
-	
-	public SunbirdApiResp searchLiveContent(String contentId) {
-		SunbirdApiResp response = null;
-		// Call Content Search API here. return the batch details
+
+	public Map<String, Object> searchLiveContent(String contentId) {
+		Map<String, Object> response = null;
+		HashMap<String, String> headerValues = new HashMap<>();
+		headerValues.put(Constants.CONTENT_TYPE, Constants.APPLICATION_JSON);
+		Map<String, Object> filters = new HashMap<>();
+		filters.put(Constants.PRIMARY_CATEGORY, Arrays.asList(Constants.COURSE, Constants.PROGRAM));
+		filters.put(Constants.STATUS, Arrays.asList(Constants.LIVE));
+		filters.put(Constants.IDENTIFIER, contentId);
+		Map<String, Object> contentRequestValue = new HashMap<>();
+		contentRequestValue.put(Constants.FILTERS, filters);
+		contentRequestValue.put(Constants.QUERY, "");
+		contentRequestValue.put(Constants.FIELDS, Arrays.asList(Constants.IDENTIFIER, Constants.NAME, Constants.PRIMARY_CATEGORY, Constants.BATCHES));
+		Map<String, Object> contentRequest = new HashMap<>();
+		contentRequest.put(Constants.REQUEST, contentRequestValue);
+		response = outboundRequestHandlerService.fetchResultUsingPost(serverConfig.getKmBaseHost() + serverConfig.getKmBaseContentSearch(), contentRequest, headerValues);
+		if (null != response && Constants.OK.equalsIgnoreCase((String) response.get(Constants.RESPONSE_CODE))) {
+			return response;
+		}
 		return null;
 	}
 }
