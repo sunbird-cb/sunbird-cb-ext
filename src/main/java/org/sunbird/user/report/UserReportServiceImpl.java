f6c4bad2c79f268859c4002d55545535cecd5f0e
karthik-tarento <karthikeyan.rajendran@tarento.com>
2022-12-23 11:12:38 +0530

    Optimized the code for user report generation

--- src/main/java/org/sunbird/user/report/UserReportServiceImpl.java
+++ src/main/java/org/sunbird/user/report/UserReportServiceImpl.java
@@ -80,42 +80,57 @@ public class UserReportServiceImpl implements UserReportService {
 				(System.currentTimeMillis() - startTime) / 1000));
 	}
 
-	public void generateUserReport(Map<String, Map<String, String>> userInfoMap, List<String> fields,
-			SBApiResponse response) {
-		log.info("UserReportServiceImpl:: generateUserReport started");
-		long startTime = System.currentTimeMillis();
-
-		Workbook wb = new XSSFWorkbook();
-		Sheet sheet = wb.createSheet("KarmayogiBharat User Details");
-		int rowNum = 0;
-		Row row = sheet.createRow(rowNum++);
-		int cellNum = 0;
-		for (String fieldName : fields) {
-			Cell cell = row.createCell(cellNum++);
-			cell.setCellValue(fieldName);
+	public Workbook createReportWorkbook(List<String> fields) {
+		try {
+			Workbook wb = new XSSFWorkbook();
+			Sheet sheet = wb.createSheet("KarmayogiBharat User Details");
+			int rowNum = 0;
+			int cellNum = 0;
+			Row row = sheet.createRow(rowNum++);
+			for (String fieldName : fields) {
+				Cell cell = row.createCell(cellNum++);
+				cell.setCellValue(fieldName);
+			}
+			return wb;
+		} catch (Exception e) {
+			log.error("Failed to create a workbook ", e);
 		}
+		return null;
+	}
 
-		Iterator<Entry<String, Map<String, String>>> it = userInfoMap.entrySet().iterator();
-		while (it.hasNext()) {
-			Entry<String, Map<String, String>> entry = it.next();
-			Row rowE = sheet.createRow(rowNum++);
-			cellNum = 0;
-			for (String field : fields) {
-				Cell cell = rowE.createCell(cellNum++);
-				String cellVal = entry.getValue().get(field);
-				if (StringUtils.isNotBlank(cellVal)) {
-					cell.setCellValue(cellVal);
-				} else {
-					cell.setCellValue("");
+	public Workbook appendData(Workbook wb, List<String> fields, Map<String, Map<String, String>> userInfoMap) {
+		int rowNum = 0;
+		int cellNum = 0;
+		try {
+			Sheet sheet = wb.getSheet("KarmayogiBharat User Details");
+			rowNum = sheet.getLastRowNum()+1;
+			Iterator<Entry<String, Map<String, String>>> it = userInfoMap.entrySet().iterator();
+			while (it.hasNext()) {
+				Entry<String, Map<String, String>> entry = it.next();
+				Row rowE = sheet.createRow(rowNum++);
+				cellNum = 0;
+				for (String field : fields) {
+					Cell cell = rowE.createCell(cellNum++);
+					String cellVal = entry.getValue().get(field);
+					if (StringUtils.isNotBlank(cellVal)) {
+						cell.setCellValue(cellVal);
+					} else {
+						cell.setCellValue("");
+					}
 				}
 			}
+		} catch (Exception e) {
+			log.error("Failed to write data into the User Report excel. Exception: ", e);
+			wb = null;
 		}
+		return wb;
+	}
 
-		String fileName = userStorePath + "/userReport-" + java.time.LocalDate.now() + System.currentTimeMillis()
-				+ ".xlsx";
-		log.info("Constructed File Name -> " + fileName);
-
+	public void completeReportWorkbook(Workbook wb, SBApiResponse response) {
 		try {
+			String fileName = userStorePath + "/userReport-" + java.time.LocalDate.now() + System.currentTimeMillis()
+					+ ".xlsx";
+			log.info("Constructed File Name -> " + fileName);
 			File file = new File(fileName);
 			file.createNewFile();
 			OutputStream fileOut = new FileOutputStream(file, false);
@@ -123,10 +138,7 @@ public class UserReportServiceImpl implements UserReportService {
 			wb.close();
 			response.getResult().put(Constants.FILE_NAME, fileName);
 		} catch (Exception e) {
-			log.error("Failed to write the workbook created for UserEnrolment Report. Exception: ", e);
+			log.error("Failed to write data into User Report excel file. Exception: e", e);
 		}
-
-		log.info(String.format("UserReportServiceImpl:: generateUserReport started and it took %s seconds to complete.",
-				(System.currentTimeMillis() - startTime) / 1000));
 	}
 }
