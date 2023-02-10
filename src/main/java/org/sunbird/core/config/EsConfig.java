7c6701a11e0abf52eb81fdd0b45f26995c0092a3
sreeragksgh <sreeragsajeesh@gmail.com>
2023-02-10 15:55:57 +0530

    test for ES IP issue

--- src/main/java/org/sunbird/core/config/EsConfig.java
+++ src/main/java/org/sunbird/core/config/EsConfig.java
@@ -12,9 +12,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.sunbird.common.util.CbExtServerProperties;
+import org.springframework.util.StringUtils;
+import org.sunbird.core.logger.CbExtLogger;
+
+import java.util.List;
+
 
 @Configuration
 public class EsConfig {
+	private CbExtLogger logger = new CbExtLogger(getClass().getName());
 	@Autowired
 	CbExtServerProperties configuration;
 
@@ -33,8 +39,21 @@ public class EsConfig {
 	private RestHighLevelClient createRestClient(String host, String port, String user, String password) {
 		final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
 		credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(user, password));
-
-		RestClientBuilder builder = RestClient.builder(new HttpHost(host, Integer.parseInt(port)))
+		List<String> hostList = null;
+		if (!StringUtils.isEmpty(host) && !StringUtils.isEmpty(port)) {
+			String[] splitHost = host.split(",");
+			logger.info("splitHost : "+splitHost.toString());
+			for (String val : splitHost) {
+                logger.info("IPS are : "+val);
+				hostList.add(val);
+			}
+		}
+		HttpHost[] httpHost = new HttpHost[hostList.size()];
+		for (int i = 0; i < hostList.size(); i++) {
+			httpHost[i] = new HttpHost(hostList.get(i), Integer.parseInt(port));
+		}
+		logger.info("httpHost is : "+httpHost.toString());
+		RestClientBuilder builder = RestClient.builder(httpHost)
 				.setHttpClientConfigCallback(
 						httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
 
