d6341b1144c4e556eb5a542801107c8496020aec
Juhi <juhiagl8@gmail.com>
2023-02-20 12:15:03 +0530

    APIs for Offensive Data Flags

--- src/main/java/org/sunbird/assessment/service/OffensiveDataFlagServiceImpl.java
+++ src/main/java/org/sunbird/assessment/service/OffensiveDataFlagServiceImpl.java
@@ -34,20 +34,20 @@ public class OffensiveDataFlagServiceImpl implements OffensiveDataFlagService {
         String userId = RequestInterceptor.fetchUserIdFromAccessToken(token);
         errMsg = validateRequest(requestBody, userId, Constants.ADD);
         SBApiResponse response = ProjectUtil.createDefaultResponse(Constants.ADD_OFFENSIVE_DATA_FLAG);
-            if (StringUtils.isEmpty(errMsg)) {
-                Map<String, Object> request = (Map<String, Object>) requestBody.get(Constants.REQUEST);
-                request.put(Constants.USER_ID, userId);
-                request.put(Constants.CONTEXT_STATUS, Constants.DRAFT);
-                request.put(Constants.CREATED_BY, userId);
-                request.put(Constants.CREATED_DATE, new Timestamp(new java.util.Date().getTime()));
-                SBApiResponse resp = cassandraOperation.insertRecord(Constants.KEYSPACE_SUNBIRD,
-                        Constants.TABLE_OFFENSIVE_DATA_FLAGS, request);
-                if (!resp.get(Constants.RESPONSE).equals(Constants.SUCCESS)) {
-                    errMsg = "Failed to add records into DB";
-                } else {
-                    response.getResult().put(Constants.STATUS, Constants.CREATED);
-                }
+        if (StringUtils.isEmpty(errMsg)) {
+            Map<String, Object> request = (Map<String, Object>) requestBody.get(Constants.REQUEST);
+            request.put(Constants.USER_ID, userId);
+            request.put(Constants.CONTEXT_STATUS, Constants.DRAFT);
+            request.put(Constants.CREATED_BY, userId);
+            request.put(Constants.CREATED_DATE, new Timestamp(new java.util.Date().getTime()));
+            SBApiResponse resp = cassandraOperation.insertRecord(Constants.KEYSPACE_SUNBIRD,
+                    Constants.TABLE_OFFENSIVE_DATA_FLAGS, request);
+            if (!resp.get(Constants.RESPONSE).equals(Constants.SUCCESS)) {
+                errMsg = "Failed to add records into DB";
+            } else {
+                response.getResult().put(Constants.STATUS, Constants.CREATED);
             }
+        }
         if (StringUtils.isNotBlank(errMsg)) {
             response.getParams().setStatus(Constants.FAILED);
             response.getParams().setErrmsg(errMsg);
@@ -65,25 +65,25 @@ public class OffensiveDataFlagServiceImpl implements OffensiveDataFlagService {
         errMsg = validateRequest(requestBody, userId, Constants.UPDATE);
         if (StringUtils.isEmpty(errMsg)) {
             try {
-                    Map<String, Object> request = (Map<String, Object>) requestBody.get(Constants.REQUEST);
-                    Map<String, Object> compositeKeys = new HashMap<>();
-                    compositeKeys.put(Constants.USER_ID, userId);
-                    compositeKeys.put(Constants.CONTEXT_TYPE, request.get(Constants.CONTEXT_TYPE));
-                    compositeKeys.put(Constants.CONTEXT_TYPE_ID, request.get(Constants.CONTEXT_TYPE_ID));
-                    Map<String, Object> fieldsToBeUpdated = new HashMap<>();
-                    fieldsToBeUpdated.put(Constants.CONTEXT_STATUS, request.get(Constants.CONTEXT_STATUS));
-                    if(!ObjectUtils.isEmpty(request.get(Constants.ADDITIONAL_PARAMS))) {
-                        fieldsToBeUpdated.put(Constants.ADDITIONAL_PARAMS, request.get(Constants.ADDITIONAL_PARAMS));
-                    }
-                    fieldsToBeUpdated.put(Constants.UPDATED_BY, userId);
-                    fieldsToBeUpdated.put(Constants.UPDATED_DATE, new Timestamp(new java.util.Date().getTime()));
-                    Map<String, Object> resp = cassandraOperation.updateRecord(Constants.KEYSPACE_SUNBIRD, Constants.TABLE_OFFENSIVE_DATA_FLAGS,
-                            fieldsToBeUpdated, compositeKeys);
-                    if (!resp.get(Constants.RESPONSE).equals(Constants.SUCCESS)) {
-                        errMsg = (String) resp.get(Constants.ERROR_MESSAGE);
-                    } else {
-                        response.getResult().put(Constants.STATUS, Constants.UPDATED);
-                    }
+                Map<String, Object> request = (Map<String, Object>) requestBody.get(Constants.REQUEST);
+                Map<String, Object> compositeKeys = new HashMap<>();
+                compositeKeys.put(Constants.USER_ID, userId);
+                compositeKeys.put(Constants.CONTEXT_TYPE, request.get(Constants.CONTEXT_TYPE));
+                compositeKeys.put(Constants.CONTEXT_TYPE_ID, request.get(Constants.CONTEXT_TYPE_ID));
+                Map<String, Object> fieldsToBeUpdated = new HashMap<>();
+                fieldsToBeUpdated.put(Constants.CONTEXT_STATUS, request.get(Constants.CONTEXT_STATUS));
+                if(!ObjectUtils.isEmpty(request.get(Constants.ADDITIONAL_PARAMS))) {
+                    fieldsToBeUpdated.put(Constants.ADDITIONAL_PARAMS, request.get(Constants.ADDITIONAL_PARAMS));
+                }
+                fieldsToBeUpdated.put(Constants.UPDATED_BY, userId);
+                fieldsToBeUpdated.put(Constants.UPDATED_DATE, new Timestamp(new java.util.Date().getTime()));
+                Map<String, Object> resp = cassandraOperation.updateRecord(Constants.KEYSPACE_SUNBIRD, Constants.TABLE_OFFENSIVE_DATA_FLAGS,
+                        fieldsToBeUpdated, compositeKeys);
+                if (!resp.get(Constants.RESPONSE).equals(Constants.SUCCESS)) {
+                    errMsg = (String) resp.get(Constants.ERROR_MESSAGE);
+                } else {
+                    response.getResult().put(Constants.STATUS, Constants.UPDATED);
+                }
             } catch (Exception e) {
                 errMsg = String.format("Failed to update an offensive flag with the details. Exception: ", e.getMessage());
                 logger.error(errMsg, e);
@@ -104,12 +104,12 @@ public class OffensiveDataFlagServiceImpl implements OffensiveDataFlagService {
         String errMsg = "";
         SBApiResponse response = ProjectUtil.createDefaultResponse(Constants.GET_OFFENSIVE_DATA_FLAG);
         if (userId != null) {
-                Map<String, Object> request = new HashMap<>();
-                request.put(Constants.USER_ID, userId);
-                List<Map<String, Object>> existingDataList = cassandraOperation.getRecordsByProperties(
-                        Constants.KEYSPACE_SUNBIRD, Constants.TABLE_OFFENSIVE_DATA_FLAGS, request, null);
-                response.getResult().put("Content", existingDataList);
-                response.getResult().put("Count", existingDataList.size());
+            Map<String, Object> request = new HashMap<>();
+            request.put(Constants.USER_ID, userId);
+            List<Map<String, Object>> existingDataList = cassandraOperation.getRecordsByProperties(
+                    Constants.KEYSPACE_SUNBIRD, Constants.TABLE_OFFENSIVE_DATA_FLAGS, request, null);
+            response.getResult().put("Content", existingDataList);
+            response.getResult().put("Count", existingDataList.size());
         } else {
             errMsg = "Invalid User Id";
         }
