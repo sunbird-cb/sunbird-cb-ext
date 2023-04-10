219cabbff4a21e1ee0b7dc7948215807f16e51be
juhi agarwal <juhiagl8@gmail.com>
2023-04-10 12:02:51 +0530

    Update UserUtilityServiceImpl.java

--- src/main/java/org/sunbird/user/service/UserUtilityServiceImpl.java
+++ src/main/java/org/sunbird/user/service/UserUtilityServiceImpl.java
@@ -236,6 +236,7 @@ public class UserUtilityServiceImpl implements UserUtilityService {
 						StringUtils.EMPTY);
 				if (!CollectionUtils.isEmpty(userData)) {
 					userRegistration.setUserName((String) userData.get(Constants.USER_NAME));
+					userRegistration.setSbOrgId((String) userData.get(Constants.ROOT_ORG_ID) );
 					retValue = updateUser(userRegistration);
 				}
 			}
