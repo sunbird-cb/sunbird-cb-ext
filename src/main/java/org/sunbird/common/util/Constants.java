6a9a3dc41a373f19b9b73b13fcc6cba480bb5dd7
Juhi <juhiagl8@gmail.com>
2023-05-10 12:16:09 +0530

    changes for tags

--- src/main/java/org/sunbird/common/util/Constants.java
+++ src/main/java/org/sunbird/common/util/Constants.java
@@ -666,9 +666,9 @@ public class Constants {
 	public static final String SUCCESSFUL_RECORDS_COUNT = "successfulRecordsCount";
 	public static final String FAILED_RECORDS_COUNT = "failedRecordsCount";
 	public static final String API_FILE_DOWNLOAD = "api.file.download";
-    public static final String TAG = "tag";
+	public static final String TAG = "tag";
 
-    private Constants() {
+	private Constants() {
 		throw new IllegalStateException("Utility class");
 	}
 
