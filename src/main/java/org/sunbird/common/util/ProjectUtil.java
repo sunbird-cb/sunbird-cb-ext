3fb2a8811167574a36728c21b106e4b82a432a24
Juhi <juhiagl8@gmail.com>
2023-05-30 22:59:40 +0530

    changes for group and external system

--- src/main/java/org/sunbird/common/util/ProjectUtil.java
+++ src/main/java/org/sunbird/common/util/ProjectUtil.java
@@ -152,7 +152,7 @@ public class ProjectUtil {
 
 	public static Boolean validateTag(List<String> tags) {
 		for (String tag : tags) {
-			if (!tag.matches("[a-zA-Z ]*")) {
+			if (!tag.matches("^[a-zA-Z]+(?: [a-zA-Z]+)*$")) {
 				return false;
 			}
 		}
@@ -160,7 +160,7 @@ public class ProjectUtil {
 	}
 
 	public static Boolean validateExternalSystemId(String externalSystemId) {
-		return externalSystemId.matches("[a-zA-Z0-9]{0,30}$");
+		return externalSystemId.matches("^(?=.{1,30}$)[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$");
 	}
 
 	public static Boolean validateExternalSystem(String externalSystem) {
