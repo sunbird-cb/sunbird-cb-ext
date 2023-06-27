e37ad4f787ca537e9c53e4e51e3dcae371e90af9
Juhi <juhiagl8@gmail.com>
2023-06-27 12:16:24 +0530

    changes for group

--- src/main/java/org/sunbird/user/service/UserUtilityServiceImpl.java
+++ src/main/java/org/sunbird/user/service/UserUtilityServiceImpl.java
@@ -633,6 +633,7 @@ public class UserUtilityServiceImpl implements UserUtilityService {
 			additionalProperties.put(Constants.EXTERNAL_SYSTEM, userRegistration.getExternalSystem());
 		}
 		profileDetails.put(Constants.ADDITIONAL_PROPERTIES, additionalProperties);
+		profileDetails.put(Constants.VERIFIED_KARMAYOGI, false);
 		requestBody.put(Constants.PROFILE_DETAILS, profileDetails);
 		request.put(Constants.REQUEST, requestBody);
 		Map<String, Object> readData = (Map<String, Object>) outboundRequestHandlerService.fetchResultUsingPatch(
