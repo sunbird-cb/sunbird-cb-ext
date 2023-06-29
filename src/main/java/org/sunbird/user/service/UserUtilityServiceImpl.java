9c0def951c3fa20f903bda54b545deb995d8e91e
juhi agarwal <juhiagl8@gmail.com>
2023-06-29 11:50:45 +0530

    Group changes 4.8.3 (#257)
    * changes for group and external system

--- src/main/java/org/sunbird/user/service/UserUtilityServiceImpl.java
+++ src/main/java/org/sunbird/user/service/UserUtilityServiceImpl.java
@@ -623,9 +623,6 @@ public class UserUtilityServiceImpl implements UserUtilityService {
 		professionalDetailsList.add(professionDetailObj);
 		profileDetails.put(Constants.PROFESSIONAL_DETAILS, professionalDetailsList);
 		Map<String, Object> additionalProperties = new HashMap<String, Object>();
-		if (!StringUtils.isEmpty(userRegistration.getGroup())) {
-			additionalProperties.put(Constants.GROUP, userRegistration.getGroup());
-		}
 		if (!CollectionUtils.isEmpty(userRegistration.getTag())) {
 			additionalProperties.put(Constants.TAG, userRegistration.getTag());
 		}
@@ -636,6 +633,8 @@ public class UserUtilityServiceImpl implements UserUtilityService {
 			additionalProperties.put(Constants.EXTERNAL_SYSTEM, userRegistration.getExternalSystem());
 		}
 		profileDetails.put(Constants.ADDITIONAL_PROPERTIES, additionalProperties);
+		profileDetails.put(Constants.VERIFIED_KARMAYOGI, false);
+		profileDetails.put(Constants.MANDATORY_FIELDS_EXISTS, false);
 		requestBody.put(Constants.PROFILE_DETAILS, profileDetails);
 		request.put(Constants.REQUEST, requestBody);
 		Map<String, Object> readData = (Map<String, Object>) outboundRequestHandlerService.fetchResultUsingPatch(
