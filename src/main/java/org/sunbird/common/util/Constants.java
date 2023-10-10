5466f24db467109c590efaf2d178040a0cdc646d
saipradeep_ravipati <saipradeep.ravipati@tarento.com>
2023-10-10 13:49:22 +0530

    Conflict of merging 4.8.5 to 4.8.6

--- src/main/java/org/sunbird/common/util/Constants.java
+++ src/main/java/org/sunbird/common/util/Constants.java
@@ -694,6 +694,7 @@ public class Constants {
 	public static final String PROFILE_APPROVAL_FIELDS_KEY = "profileApprovalFields";
 	public static final String BULK_UPLOAD_VERIFICATION_REGEX = "bulk.upload.tag.verification.regex";
 	public static final String USER_REGISTRATION_GROUP_LIST = "api.user.registration.group.list";
+
 	public static final String TABLE_ENROLLMENT_BATCH_LOOKUP = "enrollment_batch_lookup";
 	public static final String UPDATE_CONTENT_PROGRESS_SUCCESS_MSG = "Content progress is updated.";
 	public static final String UPDATE_CONTENT_PROGRESS_ERROR_MSG = "Error in updating the content progress.";
@@ -736,6 +737,7 @@ public class Constants {
 
 	public static final String TABLE_COURSE_SESSION_DETAILS="sessionDetails_v2";
 
+	public static final String EMPTY="";
 	public static final String BATCH_ALREADY_ENROLLED_MSG = "Already Enrolled in batches.";
 	public static final String BATCH_AUTO_ENROLL_ERROR_MSG = "Failed to auto enrol user in batch.";
 	public static final String BATCH_NOT_AVAILABLE_ERROR_MSG = "No Batch Available.";
@@ -744,6 +746,7 @@ public class Constants {
 	public static final String SIGNIN_LINK = "signinLink";
 	public static final String DISCOVER_LINK = "discoverLink";
 	public static final String MEETING_LINK = "meetingLink";
+
 
 	public static final String BLENDED_PROGRAM_NAME="blendedProgramName";
 
