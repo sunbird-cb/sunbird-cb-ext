58c1a91220cb121c27d86ba76e3570e2c3eac330
dkttarento <138442957+dkttarento@users.noreply.github.com>
2023-12-19 17:56:07 +0530

    fetch Karma points using userId

--- src/main/java/org/sunbird/common/util/Constants.java
+++ src/main/java/org/sunbird/common/util/Constants.java
@@ -899,6 +899,7 @@ public class Constants {
 	public static final String COURSE_APP_ICON = "appIcon";
 	public static final String CREATOR_LOGO = "creatorLogo";
     public static final String USER_KARMA_POINTS ="user_karma_points";
+	public static final String KARMA_POINTS_USER_ID = "userid";
 
     private Constants() {
 		throw new IllegalStateException("Utility class");
