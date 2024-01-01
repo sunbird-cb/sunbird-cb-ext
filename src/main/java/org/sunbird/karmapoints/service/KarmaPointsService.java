c4ab7d523bb37b55774700645de56ffbb7679755
dkttarento <138442957+dkttarento@users.noreply.github.com>
2024-01-01 12:21:26 +0530

    API claimkarmapoints

--- src/main/java/org/sunbird/karmapoints/service/KarmaPointsService.java
+++ src/main/java/org/sunbird/karmapoints/service/KarmaPointsService.java
@@ -1,5 +1,6 @@
 package org.sunbird.karmapoints.service;
 
+import org.sunbird.karmapoints.model.ClaimKarmaPointsRequest;
 import org.sunbird.karmapoints.model.KarmaPointsRequest;
 
 import java.util.Map;
@@ -7,5 +8,6 @@ import java.util.Map;
 public interface KarmaPointsService {
    public Map<String, Object> fetchKarmaPointsData(String userId, KarmaPointsRequest request) ;
    public Map<String, Object> fetchKarmaPointsUserCourse(String userId, Map<String, Object> requestBody) ;
+   public void claimKarmaPoints(ClaimKarmaPointsRequest request);
 
 }
