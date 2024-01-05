ba87eead410ad9d6f6ab3fb05f7675b8f9d6ea24
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2024-01-05 17:59:20 +0530

    SPV report Info changes (#407)
    * Made the changes for checking the OrgId in the Storage service (#382)
    
    * Made the changes for checking the OrgId in the Storage service
    
    * Storage Service code review changes
    
    * Storage Service orgId changes
    
    * Report Added for spv

--- src/main/java/org/sunbird/storage/service/StorageService.java
+++ src/main/java/org/sunbird/storage/service/StorageService.java
@@ -18,8 +18,12 @@ public interface StorageService {
 
     SBApiResponse downloadFile(String fileName);
 
-	ResponseEntity<Resource> downloadFile(String reportType, String date, String orgId, String fileName);
+	ResponseEntity<Resource> downloadFile(String reportType, String date, String orgId, String fileName, String userToken);
 
 	ResponseEntity<Map<String, Map<String, Object>>> getFileInfo(String orgId);
 	public SBApiResponse uploadFile(MultipartFile file, String cloudFolderName, String containerName) throws IOException;
+
+	ResponseEntity<?> downloadFile(String reportType, String date, String fileName, String userToken);
+
+	ResponseEntity<?> getFileInfoSpv(String userToken);
 }
