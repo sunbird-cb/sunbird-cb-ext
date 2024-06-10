12788f54ff7acf1d5eaa07fc2b2677a34e508554
Ravi Saurav <ravi.saurav@tarento.com>
2024-06-10 19:16:14 +0530

    changed approach for fetchind and storing data from redis

--- src/main/java/org/sunbird/profile/service/UserBulkUploadService.java
+++ src/main/java/org/sunbird/profile/service/UserBulkUploadService.java
@@ -477,17 +477,18 @@ public class UserBulkUploadService {
         return errList;
     }
 
-    private boolean validateFieldValue(String fieldKey, String fieldValue) throws IOException {
+    private boolean validateFieldValue(String fieldKey, String fieldValue) {
         if(redisCacheMgr.keyExists(fieldKey)){
             return !redisCacheMgr.valueExists(fieldKey, fieldValue);
         } else{
             Set<String> designationsSet = new HashSet<>();
             Map<String,Object> propertiesMap = new HashMap<>();
             propertiesMap.put(Constants.CONTEXT_TYPE, fieldKey);
-            List<Map<String, Object>> languagesList = cassandraOperation.getRecordsByProperties(Constants.KEYSPACE_SUNBIRD, Constants.TABLE_MASTER_DATA, propertiesMap, Collections.singletonList(Constants.CONTEXT_NAME));
-            if(!CollectionUtils.isEmpty(languagesList)) {
-                for(Map<String, Object> languageMap : languagesList){
-                    designationsSet.add((String)languageMap.get("contextname"));
+            List<Map<String, Object>> fieldValueList = cassandraOperation.getRecordsByProperties(Constants.KEYSPACE_SUNBIRD, Constants.TABLE_MASTER_DATA, propertiesMap, Collections.singletonList(Constants.CONTEXT_NAME));
+            if(!CollectionUtils.isEmpty(fieldValueList)) {
+                String columnName = fieldValueList.get(0).get("contextname") != null ? "contextname" : "contextName";
+                for(Map<String, Object> languageMap : fieldValueList){
+                    designationsSet.add((String)languageMap.get(columnName));
                 }
             }
             redisCacheMgr.putCacheAsStringArray(fieldKey, designationsSet.toArray(new String[0]), null);
