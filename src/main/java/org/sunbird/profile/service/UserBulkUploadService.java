dd8c806dd42821ea6c2cb7dcb883778093a478f4
ravisaurav-tarento <142487505+ravisaurav-tarento@users.noreply.github.com>
2024-06-20 10:12:29 +0530

    Modified validation message for employee Id (#613)

--- src/main/java/org/sunbird/profile/service/UserBulkUploadService.java
+++ src/main/java/org/sunbird/profile/service/UserBulkUploadService.java
@@ -555,10 +555,10 @@ public class UserBulkUploadService {
                         String employeeId = record.get(9).trim();
                         userRegistration.setEmployeeId(employeeId);
                         if (!ProjectUtil.validateEmployeeId(userRegistration.getEmployeeId())) {
-                            invalidErrList.add("Invalid Employee ID : Employee ID can contain alphanumeric characters or numeric character and have a max length of 30");
+                            invalidErrList.add("Invalid Employee ID : Employee ID can contain alphabetic, alphanumeric or numeric character(s) and have a max length of 30");
                         }
                         if (userRegistration.getEmployeeId().contains(Constants.SPACE)) {
-                            invalidErrList.add("Invalid Employee ID : Employee Id cannot contain spaces");
+                            invalidErrList.add("Employee Id cannot contain spaces");
                         }
                     }
 
